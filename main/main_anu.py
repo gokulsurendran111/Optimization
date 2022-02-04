79497195a77ed3bcc8eb8c79d7a7a85d47c210e6
gokulsurendran111 <gokulsurendran111@gmail.com>
2022-02-05 00:28:23 +0530

    Update main_anu.py

--- main/main_anu.py
+++ main/main_anu.py
@@ -29,19 +29,21 @@ def cost(DroneLoc,UserLoc):
     cos_val=0
     Ndrones=len(DroneLoc)
     Nusers=len(UserLoc)
-    print(Nusers)
+ 
     User_Connection_status = np.zeros(Nusers)
-    print(User_Connection_status)
+
     for j in range(Ndrones):
         for i in range(Nusers):
             PLij = pathloss(DroneLoc[j], UserLoc[i])
-            print(PLij)
+    
             if PLij <= gamma and User_Connection_status[i]==0:
                 User_Connection_status[i] = 1
                 cos_val = cos_val + PLij/W[i]
             elif PLij>gamma and User_Connection_status[i]==0:
                 cos_val = cos_val + PLij/W[i]
-            print(cos_val)
+            elif User_Connection_status[i]==1:
+                cos_val = cos_val + 200
+           
                 
                 
     return cos_val
